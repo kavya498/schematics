74e72114a3be41aa631f39b3e19904bca472c5ef
kavya498 <46739084+kavya498@users.noreply.github.com>
2021-02-24 12:15:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,9 @@ ibmcloud_api_key=var.ibmcloud_api_key
 }
 
 
-resource "ibm_cr_namespace" "test" {}
+resource "ibm_cr_namespace" "test" {
+  name = "test-12312"
+  }
 
 output "apikey"{
   value = var.ibmcloud_api_key
