bd9513ecd96067719d285e68ad759788713d1c25
kavya498 <46739084+kavya498@users.noreply.github.com>
2021-04-02 04:21:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-      version = "1.21.1"
+      version = ">=1.18.0"
     }
   }
 }
-
-resource "ibm_cr_namespace" "test" {
-  name = "test-12312"
-  }
+provider "ibm"{
+  region= "us-east"
+}
+resource "ibm_is_vpc" "testvpc" {
+  name = "testvpc"
+}
+resource "ibm_is_subnet" "testacc_subnet" {
+  name            = "testsubnet"
+  zone            = "us-east-1"
+  ipv4_cidr_block = "10.241.0.0/18"
+  vpc             = ibm_is_vpc.testvpc.id
+}
